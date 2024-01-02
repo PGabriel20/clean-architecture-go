4be3df75b4096b58ce394895c4e0375b66afdb63
PGabriel20 <bielporreca@gmail.com>
2024-01-02 18:19:03 -0300

    Correct port type

--- config/config.go
+++ config/config.go
@@ -6,7 +6,7 @@ type Config struct {
 	APPEnv          	string `mapstructure:"APP_ENV"`
 	DBDriver          string `mapstructure:"DB_DRIVER"`
 	DBHost            string `mapstructure:"DB_HOST"`
-	DBPort            string `mapstructure:"DB_PORT"`
+	DBPort            int16  `mapstructure:"DB_PORT"`
 	DBUser            string `mapstructure:"DB_USER"`
 	DBPassword        string `mapstructure:"DB_PASSWORD"`
 	DBName            string `mapstructure:"DB_NAME"`
