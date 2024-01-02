70e1e370865ef40294dfb06e56bcdfc8ccd0544e
PGabriel20 <bielporreca@gmail.com>
2024-01-02 17:01:42 -0300

    Added config file with viper

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	APPEnv          	string `mapstructure:"APP_ENV"`
+	DBDriver          string `mapstructure:"DB_DRIVER"`
+	DBHost            string `mapstructure:"DB_HOST"`
+	DBPort            string `mapstructure:"DB_PORT"`
+	DBUser            string `mapstructure:"DB_USER"`
+	DBPassword        string `mapstructure:"DB_PASSWORD"`
+	DBName            string `mapstructure:"DB_NAME"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	var cfg *Config
+
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	return cfg, err
+}
